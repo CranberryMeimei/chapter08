80b4355fb82e27447fad02483f57ccee1166cae9
Meimei <3282587053@qq.com>
2023-05-17 20:35:29 +0800

    hot-fix commit2

--- src/com/GitTest.java
+++ src/com/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello Git~~~~~~~~~");
         System.out.println("hello Git~~~~~~~~~");
         System.out.println("hello Git4  ~~~~~~~~~");
+        System.out.println("hot-fix test  ~~~~~~~~~");
     }
 }
