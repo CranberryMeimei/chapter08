4beba4d64ae0ba9d299d6cac46916a05b83cf585
Meimei <3282587053@qq.com>
2023-05-17 19:33:07 +0800

    second commit

---
+++ src/com/GitTest.java
@@ -0,0 +1,13 @@
+package com;
+
+import javax.crypto.spec.PSource;
+
+/**
+ * @author ·½ÔÂÃ¶
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git~~~~~~~~~");
+    }
+}
